779e053c0074a20d88add66dae19381bfa72aaf5
nowakrafal9 <nowakr99@gmail.com>
2022-05-12 21:12:47 +0200

    Modified build

--- src/data/FilterData.js
+++ src/data/FilterData.js
@@ -1,11 +1,17 @@
 let brands = [
     { "id": 1, "name": "Fadel-Hamil" },
-    { "id": 2, "name": "Medz" }
+    { "id": 2, "name": "Metz-Gleichner" },
+    { "id": 3, "name": "Kutch, D'Amore and Kautzer" },
+    { "id": 4, "name": "Kulas-Halvorson" },
+    { "id": 5, "name": "Rice, O'Kon and McClure" },
 ]
 
 let category = [
     { "id": 1, "name": "Antybiotyki" },
-    { "id": 2, "name": "Przeciwbólowe" }
+    { "id": 2, "name": "Przeciwbólowe" },
+    { "id": 3, "name": "Suplementy diety" },
+    { "id": 4, "name": "Maści" },
+    { "id": 5, "name": "Kosmetyki" },
 ]
 
 export {
